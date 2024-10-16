41086db3cec64f042d19fdc208d27bf993a77541
Thomas Kirby <tmkirby@umich.edu>
2024-10-16 19:40:57 -0400

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     experimental: {
         missingSuspenseWithCSRBailout: false,
+        appDir: true
     },
 };
-
 
 export default nextConfig;
